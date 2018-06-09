25a8aa8280e9773dba39653e96e851338553d7d9
Sperges <rnbiv45@gmail.com>
2018-06-09 17:59:15 -0400

    Add planets

--- js/app/main.js
+++ js/app/main.js
@@ -13,41 +13,39 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function buildFloor() {
-  var floorGeometry = new THREE.PlaneBufferGeometry(2000, 2000, 100, 100);
-  floorGeometry.rotateX(- Math.PI / 2);
-  // vertex displacement
-  var position = floorGeometry.attributes.position;
-  for (var i = 0; i < position.count; i++) {
-      vertex.fromBufferAttribute(position, i);
-      vertex.x += Math.random() * 20 - 10;
-      vertex.y += Math.random() * 2;
-      vertex.z += Math.random() * 20 - 10;
-      position.setXYZ(i, vertex.x, vertex.y, vertex.z);
-  }
-  floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
-  count = floorGeometry.attributes.position.count;
-  var colors = [];
-  for (var i = 0; i < count; i++) {
-      color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-      colors.push(color.r, color.g, color.b);
-  }
-  floorGeometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-  var floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors });
-  var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  scene.add(floor);
+function buildObjects() {
+
+  var planet = new Planet(
+    {size:600,subdivisions:3},
+    0xd6650a
+  );
+  planet.getMesh().position.z = -1000;
+  scene.add(planet.getMesh());
+
+  var moon = new Planet(
+    {size:60,subdivisions:3},
+    0x996841
+  );
+  moon.getMesh().position.z = -1000;
+  moon.getMesh().position.y = 1000;
+  scene.add(moon.getMesh());
 }
 
 function setupLighting(){
-  scene.background = new THREE.Color(0xffffff);
-  scene.fog = new THREE.Fog(0xffffff, 0, 750);
-  var light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
-  light.position.set(0.5, 1, 0.75);
+  scene.background = new THREE.Color(0x111111);
+  scene.fog = new THREE.Fog(0x111111, 0, 7500);
+  var light = new THREE.DirectionalLight(0xffffff,  0.75);
+  light.position.set(
+    Math.random(),
+    Math.random(),
+    Math.random(),
+  );
   scene.add(light);
+  scene.add(new THREE.AmbientLight(0x111111));
 }
 
 function init() {
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100000);
     scene = new THREE.Scene();
 
     setupLighting();
@@ -58,7 +56,7 @@ function init() {
 
     characterControls.registerKeyEvents();
 
-    buildFloor();
+    buildObjects();
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
