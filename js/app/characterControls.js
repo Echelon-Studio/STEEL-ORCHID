07391004ff7624a9dd559088c8271a517c564037
Lucas Burdell <lucasburdell@gmail.com>
2018-06-09 17:35:41 -0500

    add configs to comply with pull review

--- js/app/characterControls.js
+++ js/app/characterControls.js
@@ -9,126 +9,92 @@ class CharacterControls {
     this.cameraYaw = cameraYaw;
     this.cameraPitch = cameraPitch;
     this.controlsEnabled = true;
-    this.moveForward = false;
-    this.moveBackward = false;
-    this.moveLeft = false;
-    this.moveRight = false;
-    this.moveUp = false;
-    this.moveDown = false;
+    this.movementBools = {
+      move_forward: false,
+      move_backward: false,
+      move_left: false,
+      move_right: false,
+      move_up: false,
+      move_down: false
+    };
     this.velocity = new THREE.Vector3();
     this.direction = new THREE.Vector3();
     this.cameraDirection = new THREE.Vector3();
   }
 
-  onKeyDown(event) {
-    switch (event.keyCode) {
-      case 38: // up
-      case 87: // w
-        this.moveForward = true;
-        event.preventDefault();
-        return false;
-      case 37: // left
-      case 65: // a
-        this.moveLeft = true;
-        event.preventDefault();
-        return false;
-      case 40: // down
-      case 83: // s
-        this.moveBackward = true;
-        event.preventDefault();
-        return false;
-      case 39: // right
-      case 68: // d
-        this.moveRight = true;
-        event.preventDefault();
-        return false;
-      case 16: // shift
-        this.moveUp = true;
-        event.preventDefault();
-        return false;
-      case 17: // control
-        this.moveDown = true;
-        event.preventDefault();
-        break
-      case 32: // space
-        if (this.canJump === true) this.velocity.y += JUMP_VELOCITY;
-        this.canJump = false;
-        event.preventDefault();
-        return false;
+  getMovementTypeFromKeyCode(keyCode) {
+    for (var key in configs.controls) {
+      if (!configs.controls.hasOwnProperty(key)) {
+        continue;
+      }
+      for (var i = 0; i < configs.controls[key].length; i++) {
+        if (keyCode == configs.controls[key][i]) {
+          return key;
+        }
+      }
     }
+    return null;
   }
 
-  onKeyUp(event) {
-    switch (event.keyCode) {
-      case 38: // up
-      case 87: // w
-        this.moveForward = false;
-        event.preventDefault();
-        return false;
-      case 37: // left
-      case 65: // a
-        this.moveLeft = false;
-        event.preventDefault();
-        return false;
-      case 40: // down
-      case 83: // s
-        this.moveBackward = false;
-        event.preventDefault();
-        return false;
-      case 39: // right
-      case 68: // d
-        this.moveRight = false;
-        event.preventDefault();
-        return false;
-      case 16: // shift
-        this.moveUp = false;
-        event.preventDefault();
-        return false;
-      case 17: // control
-        this.moveDown = false;
-        event.preventDefault();
-        return false;
+  onKeyEvent(event, isKeyDown) {
+    var movementType = this.getMovementTypeFromKeyCode(event.keyCode);
+    if (this.movementBools[movementType] !== null) {
+      this.movementBools[movementType] = isKeyDown;
+      event.preventDefault();
+      return false;
     }
+    return true;
   }
-
 
   registerKeyEvents() {
     var x = this;
     document.addEventListener('keydown', function(event) {
-      x.onKeyDown(event)
+      x.onKeyEvent(event, true)
     }, false);
     document.addEventListener('keyup', function(event) {
-      x.onKeyUp(event)
+      x.onKeyEvent(event, false)
     }, false);
-    console.log("key events registered");
+    window.onbeforeunload = function() { //Prevent Ctrl+W
+      return false;
+    };
+
   }
+
+
+  updateVelocity(deltaTime) {
+    this.direction.z = Number(this.movementBools["move_forward"]) -
+      Number(this.movementBools["move_backward"]);
+    this.direction.x = Number(this.movementBools["move_left"]) -
+      Number(this.movementBools["move_right"]);
+    this.direction.y = Number(this.movementBools["move_up"]) -
+      Number(this.movementBools["move_down"]);
+    this.direction.normalize();
+
+    if (this.direction.x !== 0) {
+      var vect = new THREE.Vector3(-1, 0, 0);
+      this.velocity.add(vect.applyQuaternion(this.cameraYaw.quaternion).multiplyScalar(this.direction.x * SPEED * deltaTime));
+    }
+
+    if (this.direction.y !== 0) {
+      var vect = new THREE.Vector3(0, 1, 0);
+      this.velocity.add(vect.applyQuaternion(camera.quaternion).multiplyScalar(this.direction.y * SPEED * deltaTime));
+    }
+
+    console.log(this.movementBools["move_forward"] + " " + this.movementBools["move_backward"] + " " + this.direction.z);
+    if (this.direction.z !== 0) {
+      this.velocity.add(this.cameraDirection.multiplyScalar(this.direction.z * SPEED * deltaTime));
+    }
+  }
+
 
   update(deltaTime) {
     if (!this.controlsEnabled) {
       return;
     }
-
     camera.getWorldDirection(this.cameraDirection);
     var forward = this.cameraDirection;
 
-    this.direction.z = Number(this.moveForward) - Number(this.moveBackward);
-    this.direction.x = Number(this.moveLeft) - Number(this.moveRight);
-    this.direction.y = Number(this.moveUp) - Number(this.moveDown);
-    this.direction.normalize();
-
-
-    if (this.moveForward || this.moveBackward) {
-      this.velocity.add(forward.multiplyScalar(this.direction.z * SPEED * deltaTime));
-    }
-    if (this.moveLeft || this.moveRight) {
-      var vect = new THREE.Vector3(-1, 0, 0);
-      this.velocity.add(vect.applyQuaternion(this.cameraYaw.quaternion).multiplyScalar(this.direction.x * SPEED * deltaTime));
-    }
-    if (this.moveUp || this.moveDown) {
-      var vect = new THREE.Vector3(0, 1, 0);
-      this.velocity.add(vect.applyQuaternion(camera.quaternion).multiplyScalar(this.direction.y * SPEED * deltaTime));
-    }
-
+    this.updateVelocity(deltaTime);
 
     this.cameraYaw.position.set(
       this.cameraYaw.position.x + this.velocity.x,
---
+++ js/app/configs.js
@@ -0,0 +1,10 @@
+var configs = { };
+
+configs.controls = {
+  move_forward: [enums.keyboard.UP_ARROW, enums.keyboard.KEY_W],
+  move_backward: [enums.keyboard.DOWN_ARROW, enums.keyboard.KEY_S],
+  move_left: [enums.keyboard.LEFT_ARROW, enums.keyboard.KEY_A],
+  move_right: [enums.keyboard.RIGHT_ARROW, enums.keyboard.KEY_D],
+  move_up: [enums.keyboard.SHIFT, enums.keyboard.KEY_F],
+  move_down: [enums.keyboard.CTRL, enums.keyboard.KEY_C]
+}
